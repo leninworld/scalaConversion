98da32c75bd43ce085af72a6126f34455508ed46
lenin <lenin.world@gmail.com>
2018-08-01 01:04:57 -0400

    Wrapper sequence working. Clean up code

--- src/main/scala/Approach1.scala
+++ src/main/scala/Approach1.scala
@@ -7,12 +7,6 @@ object Approach1    {
   }
 
   case class personD(name: String, weight: Double) extends Something[String, Double]
-  case class personlstD(values: List[personD])
-
-  case class Strings(values: List[String])
-  case class Ints(values: List[Int])
-  case class Doubles(values: List[Double])
-
 
   var min = 500.0
   var max = -1.0
@@ -21,16 +15,6 @@ object Approach1    {
 
   def printType[T](x:T) :Unit = {
     println(x.getClass.toString())
-  }
-
-  def extractDouble(expectedNumber: Any): Double = expectedNumber match {
-    case i: Int => i.toDouble
-    case l: Long => l.toDouble
-    case d: Double => d
-  }
-
-  def extractString(expectedNumber: Any): String = expectedNumber match {
-    case d: String => d
   }
 
   def convert_list_to_k(in:List[Any]): Any =  {
@@ -43,12 +27,11 @@ object Approach1    {
     println("newin2:"+newin2)
     var newlst : List[Any] = List[Any]()
     if(newin1.size > 0 ){
-//      newlst: List[personD] = List[personD]()
       newin1.foreach( x  => {
 
             println("inside personI: " + x+" "+x._1 +" "+x._2)
-            var i =  x._2 * 0.45.toDouble
-            var nname = extractString(x._1 )
+            var i =  x._2 / 0.45359237
+            var nname = x._1
             var p1 = personD(nname, i)
             newlst = newlst ::: List(p1)
 
@@ -59,9 +42,8 @@ object Approach1    {
     }
 
     if(newin2.size > 0 ){
-//        newlst: List[Double] = List[Double]()
         newin2.foreach( x => {
-            var i =  x * 0.45.toDouble
+            var i =  x / 0.45359237
             newlst = newlst ::: List(i)
           }
         )
@@ -75,7 +57,8 @@ object Approach1    {
   }
 
   def find_max_and_min(in: List[Any]): Any = {
-
+    min = 500.0
+    max = -1.0
     var  newin = flatten(in)
 
     var newin1  = newin.collect{case personD(x:String, y:Double) => (x:String, y:Double)}
@@ -86,31 +69,26 @@ object Approach1    {
     println("newin2:"+newin2)
     var newlst : List[Any] = List[Any]()
     if(newin1.size > 0 ){
-//      var newlst: List[personD] = List[personD]()
       newin1.foreach( x  => {
 
         println("inside personI: " + x+" "+x._1 +" "+x._2)
 
-        var nname = extractString(x._1 )
-
         if(x._2 > max){
           max = x._2
-          max_name = nname
+          max_name = x._1
         }
         if(x._2 < min){
           min = x._2
-          min_name = nname
+          min_name = x._1
         }
-
-
       }
 
       );
+
       return (max_name, max, min_name, min)
     }
 
     if(newin2.size > 0 ){
-//      var newlst: List[Double] = List[Double]()
       newin2.foreach( x => {
         if(x > max){
           max = x
@@ -127,15 +105,12 @@ object Approach1    {
   def main(args: Array[String]): Unit = {
 
     val p1 = Approach1.personD("lenin", 90.9)
-    val p2 = Approach1.personD("name1", 70.2)
-    val p3 = Approach1.personD("name4a", 60.1)
+    val p2 = Approach1.personD("martin", 70.2)
+    val p3 = Approach1.personD("kishore", 60.1)
+    val p4 = Approach1.personD("lalitha", 68.1)
 
     var lstD = List(p1, p2 , p3)
-
-    println("------------------------")
-    // given object <name,weight>, find the min and max weight along with the person name
-//    var ret = Approach1.find_max_and_min(lstD);
-//    println("---->res1:"+ret)
+    // START - wrapper Sequence
     println("------------------------")
     // convert given a list of double weights (kgs to lbs)
     var ret = Approach1.convert_list_to_k(List[Double](111.1,211.2,111.1))
@@ -153,7 +128,7 @@ object Approach1    {
     println("------------------------")
     ret = Approach1.find_max_and_min(List(ret));
     println("---->res4:"+ret)
-
+    // END - wrapper Sequence
   }
 
 }
